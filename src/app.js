73eacb0bd4a22585da3e5747a0b4e2c7e43dfd44
Terrys101 <97230300+Terrys101@users.noreply.github.com>
2022-03-26 13:11:44 +0000

    update app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+ const hamburgerButton = document.getElementById('hamburger')
+ const navList = document.getElementById('nav-list')
+
+const toggleButton = () => {
+    navList.classList.toggle('show')
+
+}
+
+
+hamburgerButton.addEventListener('click', toggleButton)
